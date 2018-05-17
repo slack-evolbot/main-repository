a6ff5b18bf32dce284c0edac429a3c4d16d1919b
slack-evolbot <slack_evolbot@yahoo.co.jp>
2018-05-17 15:49:10 +0900

    シェルファイルによるコミット

---
+++ tool/git_commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+LOG=/home/pi/log/commit.log
+PASS='pwd'
+cd /home/pi/git/main-repository >> $LOG
+git add * >> $LOG
+if [$# -ne 1]; then
+    git commit -a -m $1 >> $git
+else
+    git commit -a -m "シェルファイルによるコミット" >> $LOG
+fi
+git pull origin master >> $LOG
+git push origin master:master >> $LOG
+cd $PASS
