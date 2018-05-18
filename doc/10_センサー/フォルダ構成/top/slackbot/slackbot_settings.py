d50ecadde41fd3bb152551a99247d4444bff7b24
Mura3go <mura3.0614@gmail.com>
2018-05-18 16:57:58 +0900

    バッチファイルによるコミット

--- doc/10_センサー/フォルダ構成/top/slackbot/slackbot_settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-API_TOKEN = 'xoxb-350517610304-oISmUNkq20Tnu7BdIsWzzGYx'
-
-# デフォルトの返答
-DEFAULT_REPLY = 'すみません、よくわかりません。Sorry'
-
-# プラグインを記述するパッケージ名のリスト
-PLUGINS = ['SlackBotPlugin']
